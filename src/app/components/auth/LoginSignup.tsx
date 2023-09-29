776b9dde1b62a0bf19eecfb23fea5fbaac8df75d
Mark O'Connor <mark.oconnor14@gmail.com>
2023-09-29 16:36:53 +0800

    feat: add user/no user state to home page and logout button

--- src/app/components/auth/LoginSignup.tsx
+++ src/app/components/auth/LoginSignup.tsx
@@ -7,13 +7,23 @@ import { useUserContext } from "@/app/UserContext";
 
 export default function LoginSignup() {
   const [isSignUp, setIsSignUp] = useState<boolean>(false);
+  const { user, logout } = useUserContext();
   return (
-    <>
+    !user ? (
+      <>
       <div>
         <button onClick={() => setIsSignUp(true)}>Sign Up</button>
         <button onClick={() => setIsSignUp(false)}>Log In</button>
       </div>
       {isSignUp ? <Signup /> : <Login />}
-    </>
+
+      </>
+    ) : (
+      <>
+      <p>You signed in!</p>
+      <p>{user.email}</p>
+      <button onClick={() => logout()}>Log Out</button>
+      </>
+    )
   );
 }
