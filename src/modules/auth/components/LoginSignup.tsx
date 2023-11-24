f63231b63773ef789ff179cc356dbea787aca11d
Mark O'Connor <mark.oconnor14@gmail.com>
2023-11-25 00:16:44 +0800

    feat: make forms look better and logged in state

--- src/modules/auth/components/LoginSignup.tsx
+++ src/modules/auth/components/LoginSignup.tsx
@@ -1,8 +1,8 @@
 "use client";
 
 import { useState } from "react";
-import Signup from "@/modules/auth/components/Signup";
-import Login from "@/modules/auth/components/Login";
+import SignupForm from "@/modules/auth/components/SignupForm";
+import LoginForm from "@/modules/auth/components/LoginForm";
 import { useUserContext } from "@/app/UserContext";
 
 export default function LoginSignup() {
@@ -10,16 +10,42 @@ export default function LoginSignup() {
   const { user, logout } = useUserContext();
   return !user ? (
     <div className="max-w-[600px] mx-auto">
-      <div className="flex gap-4">
-        <button onClick={() => setIsSignUp(true)}>Sign Up</button>
-        <button onClick={() => setIsSignUp(false)}>Log In</button>
-      </div>
-      {isSignUp ? <Signup /> : <Login />}
+      {isSignUp ? (
+        <div className="grid gap-4">
+          <h1>Create New Account</h1>
+          <p>Enter your info to make an account.</p>
+          <SignupForm />
+          <p>
+            Already a member?{" "}
+            <button
+              className="underline underline-offset-2"
+              onClick={() => setIsSignUp(false)}
+            >
+              Log In
+            </button>
+          </p>
+        </div>
+      ) : (
+        <div className="grid gap-4">
+          <h1>Welcome Back</h1>
+          <p>Log in with your email and password to use the app.</p>
+          <LoginForm />
+          <p>
+            Not a member?{" "}
+            <button
+              className="underline underline-offset-2"
+              onClick={() => setIsSignUp(true)}
+            >
+              Sign Up
+            </button>{" "}
+          </p>
+        </div>
+      )}
     </div>
   ) : (
     <div className="max-w-[600px] mx-auto">
-      <p>You signed in!</p>
-      <p>{user.email}</p>
+      <h1>Welcome back</h1>
+      <p>Have fun using the app... Whatever it does!</p>
       <button onClick={() => logout()}>Log Out</button>
     </div>
   );
