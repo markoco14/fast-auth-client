95f83ee2fbceb28cf033d79013f0844904488c56
Mark O'Connor <mark.oconnor14@gmail.com>
2023-11-24 23:58:33 +0800

    feat: set up header and nav styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,24 @@
 import Link from "next/link";
 import LoginSignup from "@/modules/auth/components/LoginSignup";
 
-
 export default function Home() {
   return (
-    <main>
-      <nav className="max-w-[600px] mx-auto">
-        <ul className="flex gap-4">
-          <li>
-            <Link href="/">Home</Link>
-          </li>
-          <li>
-            <Link href="/me">Me</Link>
-          </li>
-        </ul>
-      </nav>
-      <LoginSignup />
-    </main>
+    <>
+      <header className="mb-16 h-[48px] shadow">
+        <nav className="max-w-[600px] mx-auto flex h-full items-center">
+          <ul className="flex gap-4">
+            <li>
+              <Link href="/">Home</Link>
+            </li>
+            <li>
+              <Link href="/me">Me</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <main>
+        <LoginSignup />
+      </main>
+    </>
   );
 }
